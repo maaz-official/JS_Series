254495df469e68d94f62e76323ecfe6a819cd0b5
maaz-official <muhammadmaaz07@outlook.com>
2024-07-02 18:31:48 +0500

    Day 4 AT JavaScript

---
+++ Day_4.js
@@ -0,0 +1,18 @@
+// Variables
+
+// Let
+
+let Maaz = "Maaz"
+console.log(typeof Maaz);
+
+// Var
+
+var Maaz1 = "Maaz"
+console.log(typeof Maaz1);
+
+// Const
+
+const Number = 32
+console.log(Number);
+
+

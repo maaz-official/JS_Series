95987ea9dee22cc8b9d69adee0bab7a3bd341914
maaz-official <muhammadmaaz07@outlook.com>
2024-07-02 20:14:45 +0500

    Stack and Heap

---
+++ Day_9.js
@@ -0,0 +1,22 @@
+// stack and heap 
+
+// Stack (Primitive)
+
+// let Stack = "Maaz"
+// let Stack1 = Stack
+// Stack1 = "Maaz 1"
+// console.log(Stack);
+// console.log(Stack1);
+
+let Object = {
+    Id: 1,
+    emailId: "Maaz@gmail.com"
+
+}
+
+let Object2 = Object
+
+Object2.emailId = "maaz1"
+
+console.log(Object);
+console.log(Object2);

ea25b7e221b43563ab8b053ca9f9aa50ee32acbc
maaz-official <muhammadmaaz07@outlook.com>
2024-07-02 18:36:29 +0500

    Primitive Data Type

---
+++ Day_5.js
@@ -0,0 +1,16 @@
+// Primitive Data Type
+
+// String
+const Maaz = "Maaz"
+
+// Number
+const maazId =  12
+
+// BigInt 
+const BigInt = 12312312311455556n
+
+// Symbol
+const Id = Symbol("123")
+
+// Boolean
+const isLoggedIn = true

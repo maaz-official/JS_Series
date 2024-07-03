57c24dccc551f3f3102edd757234c3547b483f77
maaz-official <muhammadmaaz07@outlook.com>
2024-07-04 01:39:16 +0500

    Spread and rest Operator in function

---
+++ Day_16.js
@@ -0,0 +1,10 @@
+// Rest and Spread Operator
+
+// case 1 if we build shopping cart and adding unlimited numbers of items
+// then we use spread and rest operator
+
+function CartforShopping(...num1) {
+    console.log(num1);
+}
+
+CartforShopping(12, 14,15)

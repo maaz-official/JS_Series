5a7c149ada3905018ab3e880525896624472edc3
maaz-official <muhammadmaaz07@outlook.com>
2024-07-04 01:42:42 +0500

    update the Day_16

--- Day_16.js
+++ Day_16.js
@@ -3,7 +3,13 @@
 // case 1 if we build shopping cart and adding unlimited numbers of items
 // then we use spread and rest operator
 
-function CartforShopping(...num1) {
+// function CartforShopping(...num1) {
+//     console.log(num1);
+// }
+
+// CartforShopping(12, 14,15)
+
+function CartforShopping(value1, value2, ...num1) {
     console.log(num1);
 }
 

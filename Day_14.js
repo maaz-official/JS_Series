a4f96f41215c17b38612f173c2fdd21363ddebd2
maaz-official <muhammadmaaz07@outlook.com>
2024-07-04 01:06:26 +0500

    De Structuring Objects and API

---
+++ Day_14.js
@@ -0,0 +1,24 @@
+let Maaz = {
+    name: "Maaz",
+    codeNo: "22"
+}
+
+// let Maaz1 = Maaz.codeNo
+// console.log(Maaz1);
+
+const {name, codeNo} = Maaz
+
+console.log(name, codeNo);
+
+// API
+
+let Hopital = [
+    {
+        Ward: "1",
+        Room: "22H"
+    },
+    {
+        Ward: "2",
+        Room: "33H"
+    }
+]

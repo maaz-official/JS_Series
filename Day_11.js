df35a80ffbae042c1722a2efac01344ba1d0ed57
maaz-official <muhammadmaaz07@outlook.com>
2024-07-03 16:03:58 +0500

    Array Concatinate push and many more used

---
+++ Day_11.js
@@ -0,0 +1,44 @@
+// Arrays
+
+// Push 
+
+let Marvel_heros = ["Cap", "RDG", "Thor"];
+let DC_heros = ["Superman", "Flash", "Batman"];
+
+// Marvel_heros.push(DC_heros);
+
+// console.log(Marvel_heros);
+
+
+// Concatinate
+
+// let AllHeros = Marvel_heros.concat(DC_heros);
+
+// console.log(AllHeros);
+
+// Sprade
+
+// let AllHeross = [...Marvel_heros , ...DC_heros]
+
+// console.log(AllHeross);
+
+
+// Flate
+
+// let maaz = ["khan", "maaz", "Muhammad", ["m",["a",["z"]]]];
+
+// let maaz1 = maaz.flat(Infinity)
+
+// console.log(maaz1);
+
+// Shallow Copy
+
+console.log(Array.isArray("Maaz"));
+console.log(Array.from("Maaz"));
+console.log(Array.of("Maaz"));
+
+const score1 = 50
+const score2 = 60
+const score3 = 70
+
+console.log(Array.of(score1, score2, score3));

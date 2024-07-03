f91bcd9608ba5f5293e2d38b1f86cfa834da516a
maaz-official <muhammadmaaz07@outlook.com>
2024-07-04 01:56:15 +0500

    Function with object and Array

---
+++ Day_17.js
@@ -0,0 +1,28 @@
+// Function with object and Array
+
+let new1 = {
+    username: "maaz",
+    no: 22
+}
+
+function Objects1(anyobject){
+    console.log(`The name of the User is ${anyobject.username} and the no is ${anyobject.no}`);
+}
+
+// Objects1(new1)
+Objects1({
+    username: "Maaz",
+    no: 22
+})
+
+
+// Array
+
+let Maaz = ["name", "rollno", "no"];
+
+function Array1(getArray){
+    return getArray[1]
+}
+
+// console.log(Array1(Maaz));
+console.log(Array1(["maaz", "name"]));

496761cd4b08d517f5ff1ad7fce2da60e406de97
maaz-official <muhammadmaaz07@outlook.com>
2024-07-17 17:54:07 +0500

    Loop

---
+++ Day_21.js
@@ -0,0 +1,37 @@
+// Loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(`Increase ${i}`);
+//     console.log(element);
+    
+// }
+
+
+// let Maaz = [..."Maaz", ..."Khan"]
+// for (let index = 0; index < Maaz.length; index++) {
+//     const element = Maaz[index];
+//     console.log(element);
+    
+// }
+
+
+
+// Break And Continue
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (i==3) {
+//         break
+//     }
+//     console.log(element);
+    
+// }
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (i==3) {
+//         continue
+//     }
+//     console.log(element);
+    
+// }

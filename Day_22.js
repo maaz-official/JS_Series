32c704244f7b212d32dc743252fa1bd2f87fc46f
maaz-official <muhammadmaaz07@outlook.com>
2024-07-17 17:54:26 +0500

    While and Do Loop

---
+++ Day_22.js
@@ -0,0 +1,22 @@
+// While Loop
+// let maaz = 0
+// while (maaz <= 10) {
+//     console.log(`Value of Maaz is : ${maaz}`);
+//     maaz = maaz+2
+// }
+
+
+// let mYaRRAY = ["maaz", "khan", "ppp"]
+// let arr = 0
+// while (arr < mYaRRAY.length) {
+//     console.log(`The Value of Array is: ${mYaRRAY[arr]}`);
+//     arr =arr+1
+// }
+
+
+// Do While Loop
+let score = 0
+do {
+    console.log(`This is score is: ${score}`);
+    score++
+} while (score <= 10);

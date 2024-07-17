5903a9e37f80e7beb0491e9f123181ed3e2ad7b0
maaz-official <muhammadmaaz07@outlook.com>
2024-07-17 17:10:50 +0500

    IIFE

---
+++ Day_20.js
@@ -0,0 +1,13 @@
+// IIFE (Imediate Invoked Function execution)
+
+
+// Named Nefe hai
+(function Chai() {
+    console.log(`mAAZ`);
+})();
+
+
+// Unnamed Nefe hai
+( (name) => {
+    console.log(`Maaz ${name}`);
+})('Khan')
